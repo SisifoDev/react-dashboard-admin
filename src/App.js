3110269ff11bdb8ec73a14a5a3417c5c9655f13f
SisifoDev <manuelperdomo1710@icloud.com>
2023-01-03 07:38:58 -0500

    fix: :arrow_up: version downgrade in React Pro Sidebar
    version downgrade in React Pro Sidebar due to problems caused in the new version

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import { ColorModeContext, useMode } from './theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import TopBar from './views/global/Topbar'
 import { Route, Routes } from 'react-router-dom'
+import Sidebar from './views/global/Sidebar'
 import Dashboard from './views/dashboard'
+// import Team from './views/team'
+// import Invoices from './views/invoices'
+// import Contacts from './views/contacts'
+// import Bar from './views/bar'
+// import Form from './views/form'
+// import Line from './views/line'
+// import Pie from './views/pie'
+// import FAQ from './views/faq'
+// import Geography from './views/geography'
+// import Calendar from './views/calendar'
 
 function App() {
   const [theme, colorMode] = useMode()
@@ -11,10 +22,21 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
+          <Sidebar />
           <main className='content'>
             <TopBar />
             <Routes>
               <Route path='/' element={<Dashboard />} />
+              {/* <Route path='/team' element={<Team />} />
+              <Route path='/invoices' element={<Invoices />} />
+              <Route path='/contacts' element={<Contacts />} />
+              <Route path='/bar' element={<Bar />} />
+              <Route path='/form' element={<Form />} />
+              <Route path='/line' element={<Line />} />
+              <Route path='/pie' element={<Pie />} />
+              <Route path='/faq' element={<FAQ />} />
+              <Route path='/geography' element={<Geography />} />
+              <Route path='/calendar' element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
--- src/views/global/Sidebar.jsx
+++ src/views/global/Sidebar.jsx
@@ -1,7 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ProSidebar, Menu, MenuItem } from 'react-pro-sidebar'
+import 'react-pro-sidebar/dist/css/styles.css'
+import { Box, IconButton, Typography, useTheme } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { tokens } from '../../theme'
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined'
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined'
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined'
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined'
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined'
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined'
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined'
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined'
+import PieChartOutlinedIcon from '@mui/icons-material/PieChartOutlined'
+//import TimeLineOutlinedIcon from '@mui/icons-material/TimeLineOutlined'
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined'
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined'
 
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  const [selected, setSelected] = useState('Dashboard')
+
+  return (
+    <Box
+      sx={{
+        '& .pro-sidebar-inner': {
+          background: `${colors.primary[400]} !important`,
+        },
+        '& .pro-icon-wrapper': {
+          backgroundColor: 'transparent !important',
+        },
+        '& .pro-inner-item': {
+          padding: '5px 35px 5px 20px !important',
+        },
+        '& .pro-inner-item:hover': {
+          color: '#868dfb !important',
+        },
+        '& .pro-inner-item.active': {
+          color: '#6870fa !important',
+        },
+      }}
+    >
+      <ProSidebar collapsed={isCollapsed}>
+        <Menu iconShape='square'>
+          {/* LOGO AND MENU ICON */}
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: '10px 0 20px 0',
+              color: colors.grey[100],
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display='flex'
+                justifyContent='space-between'
+                alignItems='center'
+                ml='15px'
+              >
+                <Typography variant='h3' color={colors.grey[100]}>
+                  ADMINIS
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb='25px'>
+              <Box display='flex' justifyContent='center' alignItems='center'>
+                <img
+                  alt='profile-user'
+                  width='100px'
+                  height='100px'
+                  src={`https://img.freepik.com/fotos-premium/dibujos-animados-masculinos-personajes-3d-anteojos-polo-amarillo-naranja-bueno-foto-perfil_477250-8.jpg?w=826`}
+                  style={{ cursor: 'pointer', borderRadius: '50%' }}
+                />
+              </Box>
+              <Box textAlign='center'>
+                <Typography
+                  variant='h2'
+                  color={colors.grey[100]}
+                  fontWeight='bold'
+                  sx={{ m: '10px 0 0 0' }}
+                >
+                  Ed Roh
+                </Typography>
+                <Typography variant='h5' color={colors.greenAccent[500]}>
+                  VP Fancy Admin
+                </Typography>
+              </Box>
+            </Box>
+          )}
+
+          <Box paddingLeft={isCollapsed ? undefined : '10%'}></Box>
+        </Menu>
+      </ProSidebar>
+    </Box>
+  )
 }
 
 export default Sidebar
